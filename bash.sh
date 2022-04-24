63feed970254345db1e438db1c26cc034c253205
mishazawa <macintoshkrug@gmail.com>
2022-04-24 13:26:20 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,3 +3,6 @@
  
  # Remove the oldest archived journal files until the disk space they use falls below the specified size
  sudo journalctl --vacuum-size 10M
+
+ # compress PDF
+ gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf input.pdf
